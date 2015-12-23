2b0ac85322cde2d401f1205e352d8cb77762e57a
marthav <mvanorshoven@gmail.com>
2015-12-23 08:47:12 -0800

    changes to py file

--- finalproject.py
+++ finalproject.py
@@ -35,6 +35,6 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
 #        self.response.write('Hello world!')
 
-app = webapp2.WSGIApplication([
+application = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
